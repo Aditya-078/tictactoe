531c7a349bbf5672b832d4413d6ca1da7f5dfd5b
Aditya-078 <200303105146@paruluniversity.ac.in>
2022-05-25 19:49:59 +0530

    Done

--- src/History.jsx
+++ src/History.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 const History = ( {history, moveTo, currentMove} ) => {
-  return (
-    <ul>
+  return(
+     <div className='history-wrapper'>
+    <ul className='history'>
         {
             history.map((_, move) => {
               return  (
               <li key={move}>
                 <button 
-                style={{
-                    fontWeight: move === currentMove ? 'bold' : 'normal',
-                }}
+                className={` btn-move ${move === currentMove ? 'active' : ''}`}
                 type="button" onClick={ ()=> moveTo(move)}> 
                     {move === 0 ? 'Go to game start' : `Go to move #${move}` }
                     </button>
@@ -19,6 +18,7 @@ const History = ( {history, moveTo, currentMove} ) => {
         }
         
     </ul>
+    </div>
   )
 }
 
